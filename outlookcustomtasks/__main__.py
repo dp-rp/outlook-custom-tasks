058c0403e167f1d28967dffcd10bd10a66067fc8
Denver Pallis <denver.pallis@recordpoint.com>
2024-01-12 15:36:27 +1100

    move messages to target folder in outlook after confirmation

--- outlookcustomtasks/__main__.py
+++ outlookcustomtasks/__main__.py
@@ -22,8 +22,8 @@ _settings = get_settings()
 outlook = win32com.client.Dispatch('Outlook.Application').GetNamespace("MAPI")
 
 def move_messages(messages: List[win32com.client.CDispatch], target_folder: win32com.client.CDispatch):
-    # TODO: implement
-    raise NotImplementedError
+    for message in messages:
+        message.Move(target_folder)
 
 # --------------
 # --- SCRIPT ---
@@ -39,6 +39,8 @@ for folder in inbox.Folders:
 
 if not target_folder:
     raise RuntimeError("Failed to find target folder")
+else:
+    print(f"Using target folder: \"{target_folder.Name}\"")
 
 matches = []
 for message in inbox.Items:
@@ -49,9 +51,10 @@ for message in inbox.Items:
 while True:
     move_response = input(f"Found {len(matches)} matching emails. Move them to {_settings['target_folder_name']}? (Y/n) [{MOVE_RESPONSE_DEFAULT}]:")
 
+    # if response is empty, use default
     if move_response == "":
         move_response = MOVE_RESPONSE_DEFAULT
-        
+
     if move_response == "Y":
         print("moving...")
         move_messages(matches, target_folder)
