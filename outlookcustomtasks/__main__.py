52639cc6ce511b56cfbae192569f92d280e65022
Denver Pallis <denver.pallis@recordpoint.com>
2024-02-09 17:50:16 +1100

    get number of messages received per sender and sort from lowest to highest

--- outlookcustomtasks/__main__.py
+++ outlookcustomtasks/__main__.py
@@ -59,3 +59,71 @@ if match_count > 0:
             break
         else:
             print(f"{Fore.RED}Invalid response.{Style.RESET_ALL} {Fore.YELLOW}(note: input is case-sensitive){Style.RESET_ALL}")
+
+# # FIXME: below is temporary
+# matches = olc.find_messages(
+#     folder = inbox,
+#     filter_by = [
+#         lambda m: "Blog" in m.Subject 
+#     ]
+# )
+
+# match_count = len(matches)
+
+# for message in matches:
+#     print(f"Found match: \"{message.Subject}\" from [{message.SenderEmailAddress}]({message.SenderName})] at {message.ReceivedTime}")
+
+# print(f"Found {match_count} matching emails.")
+
+# collected_messages = [item for item in olc.inbox().Items]
+# print(len(collected_messages))
+# first_level_subfolder_messages = [folder.Items for folder in olc.inbox().Folders]
+
+# for item in first_level_subfolder_messages:
+#     print(item.Subject)
+
+# collected_messages += first_level_subfolder_messages
+# print(len(collected_messages))
+
+
+grouped_by_sender = {}
+for message in olc.inbox().Items:
+    try:
+        sender_email_address = message.SenderEmailAddress
+
+        if not sender_email_address in grouped_by_sender:
+            grouped_by_sender[sender_email_address] = []
+        
+        grouped_by_sender[message.SenderEmailAddress].append(message)
+
+    except:
+        print(f"{Fore.YELLOW}Warning: unknown sender email address for message with subject \"{message.Subject}\"{Style.RESET_ALL}")
+
+senders = []
+for sender, sender_messages in grouped_by_sender.items():
+    senders.append({
+        "sender": sender,
+        "messages": sender_messages,
+        "message_count": len(sender_messages)
+    })
+
+# filter out senders without at least 3 messages
+senders_at_least_3_messages = filter(
+    lambda sender: sender["message_count"] >= 3,
+    senders
+)
+
+# sort sender list by number of messages received
+senders_sorted_by_message_count = sorted(
+    senders_at_least_3_messages,
+    key=lambda sender: sender["message_count"],
+)
+
+for sender in senders_sorted_by_message_count:
+    duplicate_subject_count = 0
+    for message in sender['messages']:
+        message.Subject
+    print(f"{sender['message_count']:<3} :{sender['sender']}")
+
+test = olc.folder('Complete')
+print(test)
