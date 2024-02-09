b2e471e4497f05f77582eb5867ab72202e379043
Denver Pallis <denver.pallis@recordpoint.com>
2024-02-09 18:59:46 +1100

    update `get_settings` to handle new flexible rule definition structure

--- outlookcustomtasks/settings.py
+++ outlookcustomtasks/settings.py
@@ -6,9 +6,13 @@ def get_settings():
     with open(SETTINGS_FILEPATH, 'r') as f:
         settings = json.load(f)
         if settings["version"] == "1.0.0":
+            rules = []
+            for rule in settings["data"]["rules"]:
+                rules.append({
+                    "conditions": rule["conditions"],
+                    "actions": rule["actions"]
+                })
+
             return {
-                "target_folder_name": settings["data"]["targetFolderName"],
-                "subject_to_match": settings["data"]["subjectToMatch"],
-                "subject_start_to_match": settings["data"]["subjectStartToMatch"],
-                "sender_for_subject_start_matching": settings["data"]["senderForSubjectStartMatching"]
+                "rules": rules
             }
