236d03ebb9dc3e05f1b58904d270459e605854a7
Denver Pallis <denver.pallis@recordpoint.com>
2024-02-16 20:11:57 +1100

    turn repeated subject line check into part of new "basic_analytics" action

--- outlookcustomtasks/__main__.py
+++ outlookcustomtasks/__main__.py
@@ -5,8 +5,9 @@
 # built in
 from pprint import pprint
 from collections import Counter
+from itertools import groupby
 # site
-from colorama import Fore, Style, init as colorama_init
+from colorama import Fore, Back, Style, init as colorama_init
 from alive_progress import alive_bar as progressBar
 # package
 from outlookcustomtasks.outlook import OutlookClient, conditions as cond
@@ -46,7 +47,7 @@ def get_predicates_from_conditions(conditions):
                 predicates.append(sender_matches(condition["sender_matches"]))
                 bar()
             else:
-                raise RuntimeError("unsupported condition!")
+                raise RuntimeError("unsupported OCT condition!")
     return predicates
 
 def run_rule(rule):
@@ -68,18 +69,19 @@ def run_rule(rule):
     match_count = len(matches)
     print(f"Found {match_count} matching emails.")
     
-    action_count = len(rule["actions"])
+    if match_count > 0:
 
-    # HACK(Denver): just a quick way to do things - confirmation prompts should be based on which actions are associated with the given rule
-    # if this rule doesn't have any actions
-    if action_count < 1:
-        print("No actions! Continuing to next rule...")
-    elif action_count == 1:
-        first_action = rule["actions"][0]
-        
-        if "move_to_folder" in first_action:
-            target_folder_name = first_action["move_to_folder"]
-            if match_count > 0:
+        action_count = len(rule["actions"])
+
+        # HACK(Denver): just a quick way to do things - confirmation prompts should be based on which actions are associated with the given rule
+        # if this rule doesn't have any actions
+        if action_count < 1:
+            print("No actions! Continuing to next rule...")
+        elif action_count == 1:
+            first_action = rule["actions"][0]
+            
+            if "move_to_folder" in first_action:
+                target_folder_name = first_action["move_to_folder"]
                 while True:
                     move_response = input(f"Move {match_count} matching emails to the folder \"{target_folder_name}\"? (Y/n) [{MOVE_RESPONSE_DEFAULT}]:")
 
@@ -96,25 +98,122 @@ def run_rule(rule):
                         break
                     else:
                         print(f"{Fore.RED}Invalid response.{Style.RESET_ALL} {Fore.YELLOW}(note: input is case-sensitive){Style.RESET_ALL}")
-    else:
-        raise NotImplementedError("Multiple actions not yet supported.")
+            
+            elif "basic_analytics" in first_action:
+                # HACK: just a quick dirty implementation
+                print("\n----[ Basic Analytics ]----\n")
+                
+                # messages_grouped_by_sender = group_by_sender_email_address(olc.inbox().Items)
+
+                # senders = [
+                #     {
+                #         "email_address": sender_email_address,
+                #         "messages": sender_messages,
+                #         "message_count": len(sender_messages)
+                #     }
+                #     for sender_email_address, sender_messages
+                #     in messages_grouped_by_sender.items()
+                #     # filter out senders without at least 3 messages
+                #     if len(sender_messages) >= 3
+                # ]
+
+                # # sort sender list by number of messages received
+                # senders_sorted_by_message_count = sorted(
+                #     senders,
+                #     key=lambda sender: sender["message_count"],
+                # )
+
+                # # for each sender
+                # for sender in senders_sorted_by_message_count:
+                #     subject_counts = Counter([m.Subject for m in sender["messages"]]).most_common()
+
+                #     # print total message count for sender
+                #     print(f"({sender['message_count']:<3}): {sender['email_address']}")
+
+                #     # print total messages with same subject from same sender
+                #     for subject, _count in subject_counts:
+                #         print(f"  [{_count:<3}] {subject}")
+                        
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # GET TOP 10 IDENTICAL SUBJECTS REGARDLESS OF SENDER #
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+                LIMIT = 10
+                
+                messages_grouped_by_subject = group_by_subject(matches)
+
+                subjects = [
+                    {
+                        "subject": subject,
+                        "messages": subject_messages,
+                        "message_count": len(subject_messages)
+                    }
+                    for subject, subject_messages
+                    in messages_grouped_by_subject.items()
+                ]
+
+                subjects_sorted_by_message_count = sorted(
+                    subjects,
+                    key=lambda s: s['message_count'],
+                    reverse=True
+                )[:LIMIT]
+
+                # print(f"Top {limit} offenders for senders that have sent messages with identical subject lines:")
+                print(f"\n{Fore.BLACK}{Back.GREEN} Top {LIMIT} (or less) offenders for messages with identical subject lines (regardless of sender): {Style.RESET_ALL}\n")
+
+                for subject in subjects_sorted_by_message_count:
+                    print(f"[ {subject['message_count']:>3} x ] subject: [{subject['subject']}]")
+                    
+                print()
+                
+            else:
+                raise Exception(f"No supported OCT action names were found in defined actions")
+                
+        else:
+            raise NotImplementedError("Multiple OCT actions not yet supported.")
 
 def group_by_sender_email_address(messages):
     grouped_by_sender = {}
 
-    for message in messages:
-        # get sender's email address
-        sender_email_address = get_sender_email_address(message)
+    with progressBar(len(messages), title=f"grouping messages by sender", bar="filling") as bar:
+        for message in messages:
+            # get sender's email address
+            sender_email_address = get_sender_email_address(message)
 
-        # if no emails from sender yet
-        if not sender_email_address in grouped_by_sender:
-            # create empty array for sender messages
-            grouped_by_sender[sender_email_address] = []
+            # if no emails from sender yet
+            if not sender_email_address in grouped_by_sender:
+                # create empty array for sender messages
+                grouped_by_sender[sender_email_address] = []
 
-        # put message in group of other messages from same sender
-        grouped_by_sender[sender_email_address].append(message)
+            # put message in group of other messages from same sender
+            grouped_by_sender[sender_email_address].append(message)
+            bar()
 
     return grouped_by_sender
+
+# HACK: very similar usage to group_by_sender_email_address, should DRY this out
+def group_by_subject(messages):
+    keyfunc = lambda m: m.Subject
+
+    # grouped_by_subject = {
+    #     subject: list(subject_messages)
+    #     for subject, subject_messages
+    #     in groupby(
+    #         sorted(messages, key=keyfunc),
+    #         keyfunc
+    #     )
+    # }
+
+    grouped_by_subject = {}
+    with progressBar(title=f"grouping messages by subject", bar="filling") as bar:
+        for subject, subject_messages in groupby(
+            sorted(messages, key=keyfunc),
+            keyfunc
+        ):
+            grouped_by_subject[subject] = list(subject_messages)
+            bar()
+
+    return grouped_by_subject
 
 def get_sender_email_address(message):
     try:
@@ -158,38 +257,3 @@ for rule in _settings["rules"]:
 
 # collected_messages += first_level_subfolder_messages
 # print(len(collected_messages))
-
-# TODO(Denver): for stuff below, adapt any not already adapted logic to the "run_rule" flow so it can be
-# ... defined in the config instead of in code
-
-# TODO(Denver): make this it's own function
-messages_grouped_by_sender = group_by_sender_email_address(olc.inbox().Items)
-
-senders = [
-    {
-        "email_address": sender_email_address,
-        "messages": sender_messages,
-        "message_count": len(sender_messages)
-    }
-    for sender_email_address, sender_messages
-    in messages_grouped_by_sender.items()
-    # filter out senders without at least 3 messages
-    if len(sender_messages) >= 3
-]
-
-# sort sender list by number of messages received
-senders_sorted_by_message_count = sorted(
-    senders,
-    key=lambda sender: sender["message_count"],
-)
-
-# for each sender
-for sender in senders_sorted_by_message_count:
-    subject_counts = Counter([m.Subject for m in sender["messages"]]).most_common()
-
-    # print total message count for sender
-    print(f"({sender['message_count']:<3}): {sender['email_address']}")
-
-    # print total messages with same subject from same sender
-    for subject, _count in subject_counts:
-        print(f"  [{_count:<3}] {subject}")
